651c3a6708ec20ee758c82a5880cb2417ab56021
Claus Höfele <claus@claushoefele.com>
2016-11-20 11:16:43 +0100

    Set exit code if Node.js execution failed

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t lambda .
-docker run --rm -v "$(pwd):/app" -w /app/.build/lambda lambda node -e 'var fs = require("fs");require("./").handler(JSON.parse(fs.readFileSync("../../session_start.json", "utf8")), {}, function(e, r) {if (e) {console.error(e);} else {console.log(r);}});'
+docker run --rm -v "$(pwd):/app" -w /app/.build/lambda lambda node -e 'var fs = require("fs");require("./").handler(JSON.parse(fs.readFileSync("../../session_start.json", "utf8")), {}, function(e, r) {if (e) {console.error(e);process.exit(1);} else {console.log(r);}});'
